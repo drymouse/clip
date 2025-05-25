43c4f0bd81f9e4992d10dc99fb27d24b458bf97a
drymouse <mouse.janpa@gmail.com>
2025-05-26 04:58:32 +0900

    main.rs: support file input

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 # ファイルの内容をクリップボードに貼り付ける
-現在は標準入力のみ
+~~現在は標準入力のみ~~
+=> ファイルを指定してコピーができるようになった。
 
 ## インストール
 ```
 cargo install --path .
+```
+
+## 使い方
+```
+$ clip # input from stdin
+$ clip file # input from "file"
+$ cat file | clip # almost same with the second
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,28 @@
+use std::fs::File;
 use std::io::{stdin, Read};
+use std::env;
 use arboard::Clipboard;
 
+
 fn main() {
-    let mut stdin = stdin();
-    // let mut hundle = stdin.lock();
+    let args: Vec<String> = env::args().collect();
     let mut input = Vec::new();
 
-    match stdin.read_to_end(&mut input) {
+    let r_result = if args.len() >= 2 {
+        let mut infile = match File::open(args[1].clone()) {
+            Ok(f) => f,
+            Err(e) => {
+                eprintln!("open {}: {e}", args[1]);
+                return;
+            }
+        };
+        infile.read_to_end(&mut input)
+    } else {
+        stdin().read_to_end(&mut input)
+    };
+    
+
+    match r_result {
         Ok(_) => (),
         Err(e) => {
             eprintln!("read error: {e}");
@@ -26,12 +42,12 @@ fn main() {
         Ok(s) => match clipboard.set_text(s) {
             Ok(_) => (),
             Err(e) => {
-                eprintln!("utf8 transformation error: {e}");
+                eprintln!("clipboard set error: {e}");
                 return;
             }
         },
         Err(e) => {
-            eprintln!("clipboard set error: {e}");
+            eprintln!("utf8 transformation error: {e}");
             return;
         }
     };
